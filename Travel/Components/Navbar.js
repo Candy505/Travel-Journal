f6f09585c77255bddbe04e7dfe2641c90d83343e
Medha Bisht <87769883+Candy505@users.noreply.github.com>
2023-01-25 21:10:04 +0530

    project files uploaded

---
+++ Travel/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar()
+{
+    return(
+        <nav>
+        <img id = "logo" src= "https://cdn-icons-png.flaticon.com/512/744/744483.png" alt ="globe icon"></img>
+        <h3 id = "title" >My travel Journal</h3>
+        </nav>
+    );
+}
